38d25019d75aa4f4cf387638028b47b7730775b9
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2013-12-16 13:20:52 -0500

    Update GFF3 Name attribute with TAIR AGI to Accession mapping table; add GFF3 "Alias" attribute with Gene symbols from the `gene_aliases_20130831.txt` table; apart from removing the "protein" feature, also remove the "chromosome" feature (possibly causing the issue of not being able to request the transcript FASTA)

--- prepare_TAIR10_gff3.sh
+++ prepare_TAIR10_gff3.sh
@@ -9,17 +9,24 @@ TAIR10_RELEASE=ftp.arabidopsis.org/Genes/TAIR10_genome_release
 mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
 
 # prepare the necessary attribute data in tab-delimited files
-grep 'AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_NCBI_mapping_files/TAIR10_NCBI_REFSEQ_mapping_RNA | cut -f2,3 | python -m jcvi.formats.base reorder - 2,1 > GB.tsv
-grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,3 | perl -lane 'BEGIN { %data = (); } chomp; @line = split /\t/; $line[0] =~ /(\S+)\.\d+/; $data{$1} = $line[-1]; print; END { for $locus(keys %data) { print join "\t", $locus, $data{$locus}; } }' | sort -k1,1 > Note.tsv
-grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,4 > Curator_summary.tsv
-grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,5 > Computational_description.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_functional_descriptions | cut -f1,3 | perl -lane 'BEGIN { %data = (); } chomp; @line = split /\t/; $line[0] =~ /(\S+)\.\d+/; $data{$1} = $line[-1]; print; END { for $locus(keys %data) { print join "\t", $locus, $data{$locus}; } }' | sort -k1,1 > Note.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_functional_descriptions | cut -f1,4 > Curator_summary.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_functional_descriptions | cut -f1,5 > Computational_description.tsv
 
-sed -e "s/ | /\t/g" ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_gene_confidence_ranking/confidenceranking_gene | grep '^AT' | cut -f1,4 > conf_class.tsv
-sed -e "s/ | /\t/g" ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_gene_confidence_ranking/confidenceranking_gene | grep '^AT' | cut -f1,3 > conf_rating.tsv
+grep -P "\tmRNA\t" ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
+    | cut -f9 \
+    | cut -f2,3 -d";" --output-delimiter="	" \
+    | sed -r "s/Parent=|Name=//g" \
+    | python -m jcvi.formats.base group --groupby=0 --nouniq - \
+    | sort -k1,1 > gene_mRNA.map
+
+cut -f1,2 ${AIP_HOME}/${TAIR_DATA}/Genes/gene_aliases_20130831.txt | python -m jcvi.formats.base group --groupby=0 --groupsep="," - | grep -P '^AT[A-z0-9]G' | sort -k1,1 | python -m jcvi.formats.base join --noheader gene_mRNA.map - | grep -vP "na" | cut -f1,2,4  | perl -lane 'BEGIN { %data = (); } chomp; @line = split /\t/; @mrna = split /,/, $line[1]; print join "\t", $line[0], $line[-1]; foreach $m(@mrna) { print join "\t", $m, $line[-1]; }' > Alias.tsv
+
+python -m jcvi.formats.base reorder ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_TAIRAccessionID_AGI_mapping.txt 2,1 | grep -P '^AT[A-z0-9]G' | python -m jcvi.formats.base group --groupby=0 - --nouniq | sort -k1,1 | python -m jcvi.formats.base join --noheader gene_mRNA.map - | cut -f2,4 | python -m jcvi.formats.base flatten --sep="	" --zipflatten="," - | sed -e "s/,/\t/g" > Name.tsv
 
 # prepare the enriched GFF3 file with the required attributes
 python -m jcvi.formats.gff format --gff3 --unique --nostrict \
-    --remove_feat="protein" --verifySO="resolve" --dbxref="GB.tsv" \
-    --add_attribute="Note.tsv,Curator_summary.tsv,Computational_description.tsv,conf_class.tsv,conf_rating.tsv" \
+    --remove_feat="protein,chromosome" --verifySO="resolve" \
+    --add_attribute="Alias.tsv,Note.tsv,Name.tsv,Curator_summary.tsv,Computational_description.tsv" \
    ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
-   -o TAIR10_GFF3_genes_transposons.gff3 2> format.gff.log
+   -o TAIR10_GFF3_genes_transposons.gff 2> format.gff.log
