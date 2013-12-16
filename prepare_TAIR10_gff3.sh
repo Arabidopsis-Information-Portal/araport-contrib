b900f1b822a6c87ae8cec3bb35d1e070a0a012fa
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2013-12-16 12:17:59 -0500

    Added in attributes based on the different columns in the TAIR10_functional_descriptions file; added in confidence class specific attributes and GenBank Dbxref information

--- prepare_TAIR10_gff3.sh
+++ prepare_TAIR10_gff3.sh
@@ -8,41 +8,18 @@ TAIR10_RELEASE=ftp.arabidopsis.org/Genes/TAIR10_genome_release
 ## Make custom TAIR10_genome release directory
 mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
 
-## Parse the functional description table and pick the appropriate columns
-## to generate a 2-column tab delimited file (first column is equal to the
-## ID features in the GFF3 files)
-perl -lane '
-    BEGIN { %data = (); }
-    chomp;
-    next unless /^AT/;
-    @line = split /\t/;
-    $val = ($line[2] eq "") ? $line[4] : $line[2];
-    if($val =~ /;/) {
-        @valp = split /;/, $val; $val = $valp[0];
-    }
-    $val = $line[3] if($val eq "");
-    $val = "unknown protein" if($val =~ /^\D+ IN:/ or $val =~ /^CONTAINS /);
-    ($locus, $isoform) = $line[0] =~ /^(\S+)\.(\d+)$/;
-    if($isoform == 1) {
-        $data{$locus} = $val;
-        print join "\t", $locus, $val;
-    }
-    print join "\t", $line[0], $data{$locus};
-    ' \
-        ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_functional_descriptions \
-        > ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_functional_descriptions.tsv
+# prepare the necessary attribute data in tab-delimited files
+grep 'AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_NCBI_mapping_files/TAIR10_NCBI_REFSEQ_mapping_RNA | cut -f2,3 | python -m jcvi.formats.base reorder - 2,1 > GB.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,3 | perl -lane 'BEGIN { %data = (); } chomp; @line = split /\t/; $line[0] =~ /(\S+)\.\d+/; $data{$1} = $line[-1]; print; END { for $locus(keys %data) { print join "\t", $locus, $data{$locus}; } }' | sort -k1,1 > Note.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,4 > Curator_summary.tsv
+grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_functional_descriptions | cut -f1,5 > Computational_description.tsv
 
-## Run the gff formatter script to fix the following issues:
-## 1) Invent IDs for features that do not have an ID attribute
-## 2) Make all the IDs unique, within the scope of the file
-## 3) Add a note feature using the table created in the step above
-## 4) Remove the "protein" feature (which is unwanted and now replaced in the SO by the term "polypeptide")
-## 5) Verify feature types against the SO to resolve any unknown types (example: mRNA_TE_gene -> mRNA)
-## Use code from the commit: https://github.com/tanghaibao/jcvi/commit/aaa6ab3
-python -m jcvi.formats.gff format \
-    --gff3 --unique --nostrict \
-    --note=TAIR10_functional_descriptions.tsv \
-    --remove_feat="protein" --verifySO="resolve" \
-    ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
-    -o ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
-    2> TAIR10_GFF3_genes_transposons.gff.format.log
+sed -e "s/ | /\t/g" ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_gene_confidence_ranking/confidenceranking_gene | grep '^AT' | cut -f1,4 > conf_class.tsv
+sed -e "s/ | /\t/g" ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_genome_release/TAIR10_gene_confidence_ranking/confidenceranking_gene | grep '^AT' | cut -f1,3 > conf_rating.tsv
+
+# prepare the enriched GFF3 file with the required attributes
+python -m jcvi.formats.gff format --gff3 --unique --nostrict \
+    --remove_feat="protein" --verifySO="resolve" --dbxref="GB.tsv" \
+    --add_attribute="Note.tsv,Curator_summary.tsv,Computational_description.tsv,conf_class.tsv,conf_rating.tsv" \
+   Genes/TAIR10_genome_release/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
+   -o TAIR10_GFF3_genes_transposons.gff3 2> format.gff.log
