a27b95ed59a3fb8718f675b425222536771741a6
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2013-12-30 17:53:00 -0500

    Added new custom GFF file based on `Spliced_Junctions_clustered.gff`, updated `prepare_custom_GFF.sh` accordingly; added dependency information to the README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ araport-contrib
 ===============
 
 Data files (and scripts used to generate these files) for the AIP Intermine, JBrowse and GBrowse2 installations
+
+Shell scripts used to generate the data files rely on the following github repository: [tanghaibao/jcvi](https://github.com/tanghaibao/jcvi>).
+Within this repository, the [formats/gff.py](https://github.com/tanghaibao/jcvi/blob/master/formats/gff.py) module is the most heavily used, which contains a wide variety of GFF manipulation scripts.
--- prepare_custom_GFF.sh
+++ prepare_custom_GFF.sh
@@ -3,18 +3,28 @@
 ## Set Path variables
 AIP_HOME=/usr/local/projects/AIP
 TAIR_DATA=DataProviders/TAIR
+TAIR10_RELEASE=ftp.arabidopsis.org/Genes/TAIR10_genome_release
 TAIR9_RELEASE=ftp.arabidopsis.org/Genes/TAIR9_genome_release
 
-## Prepare data directories
-mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF
-mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Community_annotation_GFF
+TAIR9_gff3=${AIP_HOME}/${TAIR_DATA}/${TAIR9_RELEASE}/TAIR9_gff3
+TAIR9_custom_gff3=${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3
+mkdir -p $TAIR9_custom_gff3/Expression_GFF
+mkdir -p $TAIR9_custom_gff3/Community_annotation_GFF
 
-python -m jcvi.formats.gff format --gff --unique ${AIP_HOME}/${TAIR_DATA}/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff > ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff
+python -m jcvi.formats.gff format --gff --unique $TAIR9_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff
 
-sed -e "s/quesneville_//g" ${AIP_HOME}/${TAIR_DATA}/${TAIR9_RELEASE}/TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff > ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff
+sed -e "s/quesneville_//g" $TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff > $TAIR9_custom)gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff
 
-python -m jcvi.formats.gff chain ${AIP_HOME}/${TAIR_DATA}/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff
+python -m jcvi.formats.gff chain $TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_atproteometair7.gff
 
-sed -e "s/_match2/_match/g" ${AIP_HOME}/${TAIR_DATA}/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff > tmp$$.gff3 \
-    && python -m jcvi.formats.gff chain tmp$$.gff3 > ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
+sed -e "s/_match2/_match/g" $TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff > tmp$$.gff3 \
+    && python -m jcvi.formats.gff chain tmp$$.gff3 -o $TAIR9_custom_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
+    && rm tmp$$.gff3
+
+TAIR10_gff3=${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3
+TAIR10_custom_gff3=${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
+mkdir -p $TAIR10_custom_gff3
+
+sed -e "s/exon/match/g" $TAIR10_gff3/Spliced_Junctions_clustered.gff > tmp$$.gff3 \
+    && python -m jcvi.formats.gff chain --transfer_attrib="Note,Overlap" tmp$$.gff3 -o $TAIR10_custom_gff3/Spliced_Junctions_clustered.gff \
     && rm tmp$$.gff3
