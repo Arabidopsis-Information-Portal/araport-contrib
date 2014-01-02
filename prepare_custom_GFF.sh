80827c7ed8cfab6f18919b4762a9ab1479313105
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-01-02 14:04:35 -0500

    Updated some of the GFF files by transferring attributes from the child features into the chained parent feature (which is a necessary entity for JBrowse to rendered multipart features); updated the shell script used to generate these files

--- prepare_custom_GFF.sh
+++ prepare_custom_GFF.sh
@@ -11,20 +11,23 @@ TAIR9_custom_gff3=${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR9_RELEASE}/TAIR9_gf
 mkdir -p $TAIR9_custom_gff3/Expression_GFF
 mkdir -p $TAIR9_custom_gff3/Community_annotation_GFF
 
+## TMP filename
+TMP=tmp$$
+
 python -m jcvi.formats.gff format --gff --unique $TAIR9_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_smallRNA_17_summary.gff
 
-sed -e "s/quesneville_//g" $TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff > $TAIR9_custom)gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff
+sed -e "s/quesneville_//g" $TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff > $TAIR9_custom_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff
 
-python -m jcvi.formats.gff chain $TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_atproteometair7.gff
+python -m jcvi.formats.gff chain --transfer_attrib="minscore,maxscore,Dbxref,Note,Alias" $TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_atproteometair7.gff
 
-sed -e "s/_match2/_match/g" $TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff > tmp$$.gff3 \
-    && python -m jcvi.formats.gff chain tmp$$.gff3 -o $TAIR9_custom_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
-    && rm tmp$$.gff3
+sed -e "s/_match2/_match/g" $TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff > $TMP.gff3 \
+    && python -m jcvi.formats.gff chain --transfer_attrib="Name,FullPeptide,Translation" $TMP.gff3 -o $TAIR9_custom_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
+    && rm $TMP.gff3
 
 TAIR10_gff3=${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3
 TAIR10_custom_gff3=${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
 mkdir -p $TAIR10_custom_gff3
 
-sed -e "s/exon/match/g" $TAIR10_gff3/Spliced_Junctions_clustered.gff > tmp$$.gff3 \
-    && python -m jcvi.formats.gff chain --transfer_attrib="Note,Overlap" tmp$$.gff3 -o $TAIR10_custom_gff3/Spliced_Junctions_clustered.gff \
-    && rm tmp$$.gff3
+sed -e "s/exon/match/g" $TAIR10_gff3/Spliced_Junctions_clustered.gff > $TMP.gff3 \
+    && python -m jcvi.formats.gff chain --transfer_attrib="Name,Note,Overlap" $TMP.gff3 -o $TAIR10_custom_gff3/Spliced_Junctions_clustered.gff \
+    && rm $TMP.gff3
