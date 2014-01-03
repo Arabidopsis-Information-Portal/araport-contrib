875fbbfd20b06bf776ff32bbf9aebb029fc92733
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-01-02 23:50:36 -0500

    Transfer the score from the child features to the chained parent feature, for features with multiple parts, the `mean` of the score is computed and associated with the parent

--- prepare_custom_GFF.sh
+++ prepare_custom_GFF.sh
@@ -18,10 +18,10 @@ python -m jcvi.formats.gff format --gff --unique $TAIR9_gff3/Expression_GFF/tair
 
 sed -e "s/quesneville_//g" $TAIR9_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff > $TAIR9_custom_gff3/Community_annotation_GFF/tair9_Quesneville_Transposons_20090429.gff
 
-python -m jcvi.formats.gff chain --transfer_attrib="minscore,maxscore,Dbxref,Note,Alias" $TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_atproteometair7.gff
+python -m jcvi.formats.gff chain --transfer_attrib="minscore,maxscore,Dbxref,Note,Alias" --transfer_score="mean" $TAIR9_gff3/Expression_GFF/tair9_atproteometair7.gff > $TAIR9_custom_gff3/Expression_GFF/tair9_atproteometair7.gff
 
 sed -e "s/_match2/_match/g" $TAIR9_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff > $TMP.gff3 \
-    && python -m jcvi.formats.gff chain --transfer_attrib="Name,FullPeptide,Translation" $TMP.gff3 -o $TAIR9_custom_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
+    && python -m jcvi.formats.gff chain --transfer_attrib="Name,FullPeptide,Translation" --transfer_score="mean" $TMP.gff3 -o $TAIR9_custom_gff3/Expression_GFF/tair9_Briggs_atproteome7_20090401.gff \
     && rm $TMP.gff3
 
 TAIR10_gff3=${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3
