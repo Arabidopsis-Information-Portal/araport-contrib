671dd58017e0c4a8e18717dfe2f4186078f1960a
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-02-07 19:19:29 -0500

    Merged shared exons/CDS/UTRs shared by multi parents in TAIR10_GFF3_genes_transposons.gff; created `araport.env` (bash-compatible) to track important ENV variables, imported by all shell scripts; now storing md5sum and path to FASTA files too large to commit to repo

--- prepare_TAIR10_fasta.sh
+++ prepare_TAIR10_fasta.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-## Set Path variables
-AIP_HOME=/usr/local/projects/AIP
-TAIR_DATA=DataProviders/TAIR
-CHRS_FASTA=ftp.arabidopsis.org/Sequences/whole_chromosomes
+## set path variables (source araport env)
+source araport.env
 
-## Prepare custom data directory using the same folder structure as TAIR ftp
+## Prepare custom Chromosome FASTA data directory using the same folder structure as TAIR ftp
 mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${CHRS_FASTA}
+cd ${AIP_HOME}/${TAIR_DATA}/custom_data/${CHRS_FASTA}
 
 ## Concatenate all chromosome and organelle FASTA into one file
 ## Fix fasta headers to match the reference sequence identifiers in the GFF3 file
-cat ${AIP_HOME}/${TAIR_DATA}/${CHRS_FASTA}/TAIR10_chr?.fas \
-    | sed -e "s/chloroplast/ChrC/g" -e "s/mitochondria/ChrM/g" \
-    > ${AIP_HOME}/${TAIR_DATA}/custom_data/${CHRS_FASTA}/TAIR10_genome.fas
+sed -e "s/^>chloroplast/>ChrC/g" -e "s/^>mitochondria/>ChrM/g" \
+    ${AIP_HOME}/${TAIR_DATA}/${CHRS_FASTA}/TAIR10_chr?.fas \
+    > TAIR10_Chrs.fas
+
+## save md5sum to a txt file to track in github (since file is too large to version control)
+md5sum $PWD/TAIR10_Chrs.fas > TAIR10_Chrs.fas.md5sum
+
+## Prepare custom CDS FASTA data directory using the same folder structure as TAIR ftp
+mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${CDS_FASTA}
+cd ${AIP_HOME}/${TAIR_DATA}/custom_data/${CDS_FASTA}
+
+## Prepare the TAIR10 CDS fasta file (fix the chromosome location identifiers)
+sed -e "s/chr\([0-9A-Z]\):/Chr\1:/g" -e "s/\*/-/g" ${AIP_HOME}/${TAIR_DATA}/${CDS_FASTA}/TAIR10_cds_20101214_updated \
+    > TAIR10_cds_20101214_updated
+
+## save md5sum to a txt file to track in github (since file is too large to version control)
+md5sum $PWD/TAIR10_cds_20101214_updated > TAIR10_cds_20101214_updated.md5sum
--- prepare_TAIR10_gff3.sh
+++ prepare_TAIR10_gff3.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-## Set Path variables
-AIP_HOME=/usr/local/projects/AIP
-TAIR_DATA=DataProviders/TAIR
-TAIR10_RELEASE=ftp.arabidopsis.org/Genes/TAIR10_genome_release
+## set Path variables (source araport env)
+source araport.env
 
-## Make custom TAIR10_genome release directory
+## make custom TAIR10_genome release directory
 mkdir -p ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
+cd ${AIP_HOME}/${TAIR_DATA}/custom_data/${TAIR10_RELEASE}/TAIR10_gff3
 
 # prepare the necessary attribute data in tab-delimited files
 grep '^AT' ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_functional_descriptions | cut -f1,3 | perl -lane 'BEGIN { %data = (); } chomp; @line = split /\t/; $line[0] =~ /(\S+)\.\d+/; $data{$1} = $line[-1]; print; END { for $locus(keys %data) { print join "\t", $locus, $data{$locus}; } }' | sort -k1,1 > Note.tsv
@@ -25,8 +24,9 @@ cut -f1,2 ${AIP_HOME}/${TAIR_DATA}/Genes/gene_aliases_20130831.txt | python -m j
 python -m jcvi.formats.base reorder ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_TAIRAccessionID_AGI_mapping.txt 2,1 | grep -P '^AT[A-z0-9]G' | python -m jcvi.formats.base group --groupby=0 - --nouniq | sort -k1,1 | python -m jcvi.formats.base join --noheader gene_mRNA.map - | cut -f2,4 | python -m jcvi.formats.base flatten --sep="	" --zipflatten="," - | sed -e "s/,/\t/g" > Gene.tsv
 
 # prepare the enriched GFF3 file with the required attributes
-python -m jcvi.formats.gff format --gff3 --unique --nostrict \
-    --remove_feat="protein,chromosome" --verifySO="resolve" --dbxref="Gene.tsv" \
+python -m jcvi.formats.gff format --gff3 --unique --nostrict --multiparents="merge" \
+    --remove_feat="protein,chromosome" --verifySO="resolve" --add_dbxref="Gene.tsv" \
     --add_attribute="Alias.tsv,Note.tsv,Curator_summary.tsv,Computational_description.tsv" \
    ${AIP_HOME}/${TAIR_DATA}/${TAIR10_RELEASE}/TAIR10_gff3/TAIR10_GFF3_genes_transposons.gff \
-   -o TAIR10_GFF3_genes_transposons.gff 2> format.gff.log
+   2> format.gff.log | python -m jcvi.formats.gff sort --method="topo" stdin \
+   -o TAIR10_GFF3_genes_transposons.gff
