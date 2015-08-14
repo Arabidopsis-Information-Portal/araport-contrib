7bab7d3c91e2b90a2213efda711f46d884a3902f
Vivek Krishnakumar <vkrishna@jcvi.org>
2015-08-14 11:56:41 -0400

    + Update GFF file with June 2014 public data release from TAIR + Update supporting tsv files (Alias, Note, Curator_summary, etc.) + Update gene_association.tair file to latest releast (06/30/2015)

--- prepare_TAIR10_gff3.sh
+++ prepare_TAIR10_gff3.sh
@@ -51,12 +51,14 @@ cut -f1,3 ${LOCUS_PRIMARY_SYM} \
     | perl -lane 'chomp; @l = split /\t/; print join "\t", $l[0], $l[2]; foreach $m(split /,/, $l[1]) { print join "\t", $m, $l[2]; }' \
     > Name.tsv
 
-cut -f1,2 ${GENE_ALIASES} \
+perl -lane 'chomp; @l = split /\t/; if(scalar @l == 2) { print; } else { $desc = $l[2]; $desc =~ s/,/%2C/gs; $desc =~ s/\"//gs; print join "\t", $l[0], "$l[1]~~~$desc"; }' ${GENE_ALIASES} \
+    | cut -f1,2 \
     | python -m jcvi.formats.base group --groupby=0 --groupsep="," --nouniq - \
     | grep -P '^AT[A-z0-9]G' | sort -k1,1 \
     | python -m jcvi.formats.base join --noheader gene_transcript.map ${LOCUS_PRIMARY_SYM} - \
-    | grep -vP "\tna" | cut -f1,2,4,7 \
-    | perl -lane 'chomp; @l = split /\t/; %aliases = map { $_ => 1 } (split /,/, $l[-1]); $l[2] = undef if($l[2] eq "na"); delete $aliases{$l[2]}; $alias = join ",", sort keys %aliases; print join "\t", $l[0], $alias if($alias ne ""); foreach $m(split /,/, $l[1]) { print join "\t", $m, $alias if($alias ne ""); }' \
+    | grep -vP "\tna$" | cut -f1,2,4,7 \
+    | perl -lane 'chomp; @l = split /\t/; %aliases = map { $_ => 1 } (split /,/, $l[-1]); $l[2] = undef if($l[2] eq "na"); foreach $k(keys %aliases) { delete $aliases{$k} if($k =~ /^$l[2]~~~/); } $alias = join ",", sort keys %aliases; print join "\t", $l[0], $alias if($alias ne ""); foreach $m(split /,/, $l[1]) { print join "\t", $m, $alias if($alias ne ""); }' \
+    | sed -e "s/~~~/,/g" \
     > Alias.tsv
 
 python -m jcvi.formats.base reorder ${NCBI_AGI_MAPPING} 2,1 \
